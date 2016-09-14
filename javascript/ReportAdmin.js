b29ebcce3661e462df7f0e32197c337577fd8a46
Justin Hummel <justin.s.hummel@gmail.com>
2016-09-14 19:23:39 +0000

    Fixed ReportAdmin.js posting invalid data

--- javascript/ReportAdmin.js
+++ javascript/ReportAdmin.js
@@ -3,13 +3,23 @@
  */
 
 (function($) {
+	function paramsToObject(params) {
+		var newParamsObject = {};
+
+		$.each(params, function(index, pair) {
+			newParamsObject[pair.name] = pair.value;
+		});
+
+		return newParamsObject;
+	}
+
 	$.entwine('ss', function($){
 		$('.ReportAdmin .cms-edit-form').entwine({
 			onsubmit: function(e) {
 				var url = $.path.parseUrl(document.location.href).hrefNoSearch, 
 					params = this.find(':input[name^=filters]').serializeArray();
 				params = $.grep(params, function(param) {return (param.value);}); // filter out empty
-				if(params) url = $.path.addSearchParams(url, params);
+				if(params) url = $.path.addSearchParams(url, paramsToObject(params));
 				$('.cms-container').loadPanel(url);
 				return false;
 			}
